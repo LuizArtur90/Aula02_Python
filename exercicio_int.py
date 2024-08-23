e1fccc0d2864ae5618d337074ce8fa45785f82af
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-23 15:59:23 -0300

    salvando exercicios int

---
+++ exercicio_int.py
@@ -0,0 +1,13 @@
+#Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
+#numero_1 = int(input("Usuário, entre com o primeiro numero inteiro:"))
+#numero_2 = int(input("Usuário, entre com o segundo numero inteiro:"))
+#resultado = numero_1//numero_2
+#print(resultado)
+
+#Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero1 = float(input("Entre com o primeiro numero:"))
+numero2 = float(input("Entre com o segundo numero:"))
+resultado = numero1 * numero2
+print(resultado)
