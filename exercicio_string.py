76e55fee6f5868fbf2f5b683b934e919b50b1a7a
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-23 17:48:27 -0300

    atualizando exercicio string

---
+++ exercicio_string.py
@@ -0,0 +1,13 @@
+#Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+
+#data_input = input("Usuários, entre com a data no formato dd/mm/aaaa por favor:")
+#dia = data_input.split("/")
+#print(f" O dia é {dia[0]}, o mês é {dia[1]} e o ano é {dia[2]}.")
+
+
+#Escreva um programa que concatene duas strings fornecidas pelo usuário.
+
+string1 = input("Entre com a primeira string:")
+string2 = input("Entre com a segunda string:")
+conc_string = string1 + string2
+print(conc_string)
