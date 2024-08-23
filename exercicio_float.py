f3b7ce23ca42dd25e0aee63b716dc7b444ae0efc
Luiz Artur <luiz.lamoglia@gmail.com>
2024-08-23 16:34:56 -0300

    atualziando exercicio float

---
+++ exercicio_float.py
@@ -0,0 +1,12 @@
+#Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
+
+#numero = float(input("Usuário, por favor entre com o valor numérico:"))
+#expoente = float(input("Usuário, por favor entre com o valor do expoente:"))
+#resultado = numero**expoente
+#print(f"O resultado final é:" , resultado)
+
+#Faça um programa que converta a temperatura de Celsius para Fahrenheit.
+
+#celsius = float(input("Entre com o valor em graus Celsius:"))
+#fahrenheit = (celsius*1.8) + 32
+#print(f"A temperatura de {celsius} ºC equivale a {fahrenheit}ºF")
